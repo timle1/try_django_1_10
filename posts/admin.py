f7eda3252d84504d1100b9ac32fec1fc0a21c32c
Your Name Nobody <you@example.com>
2016-10-07 22:36:06 -0400

    from conf.urls import include for app.urls

--- posts/admin.py
+++ posts/admin.py
@@ -5,16 +5,15 @@ from posts.models import Post
 
 
 class PostModelAdmin(admin.ModelAdmin):
-    list_display = ["title", "updated", "timestamp"]
-    list_display_links = ["updated"]
+    list_display = ["title", "last_updated", "init_timestamp"]
+    list_display_links = ["last_updated"]
     list_editable = ["title"]
-    list_filter = ["updated", "timestamp"]
+    list_filter = ["last_updated", "init_timestamp"]
 
     search_fields = ["title", "content"]
 
     class Meta:
         model = Post
-
 
 
 admin.site.register(Post, PostModelAdmin)

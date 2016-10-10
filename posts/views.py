b4f933c05f06decfec8c8f05d1fe570cc1cded55
Your Name Nobody <you@example.com>
2016-10-10 19:35:24 -0400

    18, parameter in url

--- posts/views.py
+++ posts/views.py
@@ -9,8 +9,8 @@ def post_create(request):
     return HttpResponse("<h1>Create</h1>")
 
 
-def post_detail(request):  # retrieve
-    instance = get_object_or_404(Post, id=3)
+def post_detail(request, id=None):  # retrieve
+    instance = get_object_or_404(Post, id=id)
     context = {
         'title': 'Detail',
         'instance': instance,

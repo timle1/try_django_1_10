9db86050949765ebaae37300a73c8d9c860c9254
Your Name Nobody <you@example.com>
2016-10-10 21:11:56 -0400

    19, Dynamic url routing

--- posts/models.py
+++ posts/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.urlresolvers import reverse
+
 
 # Create your models here.
 class Post(models.Model):
@@ -9,3 +11,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('posts:detail', kwargs={'id': self.id})

242f6606599a474819ea7d825df5cc0f65a80982
Your Name Nobody <you@example.com>
2016-10-11 13:40:57 -0400

    21, update view

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
@@ -9,8 +9,8 @@ def post_create(request):
     form = PostForm(request.POST or None)
     if form.is_valid():
         instance = form.save(commit=False)
-        print(form.cleaned_data.get('title'))
         instance.save()
+        return HttpResponseRedirect(instance.get_absolute_url())
     context = {
         'form': form
     }
@@ -20,7 +20,7 @@ def post_create(request):
 def post_detail(request, id=None):  # retrieve
     instance = get_object_or_404(Post, id=id)
     context = {
-        'title': 'Detail',
+        'title': instance.title,
         'instance': instance,
     }
     return render(request, 'post_detail.html', context)
@@ -35,8 +35,19 @@ def post_list(request):  # list items
     return render(request, 'index.html', context)
 
 
-def post_update(request):
-    return HttpResponse("<h1>Update</h1>")
+def post_update(request, id=None):
+    instance = get_object_or_404(Post, id=id)
+    form = PostForm(request.POST or None, instance=instance)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        return HttpResponseRedirect(instance.get_absolute_url())
+    context = {
+        'title': instance.title,
+        'instance': instance,
+        'form': form,
+    }
+    return render(request, 'post_form.html', context)
 
 
 def post_delete(request):

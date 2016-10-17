d52b55fc04c90807c91440beb6cf24b01f1204c2
Tim Le <TimLe1@Live.com>
2016-10-17 16:08:01 -0400

    AtB 20, create reading time blurb in post details

--- posts/models.py
+++ posts/models.py
@@ -10,6 +10,7 @@ from django.contrib.contenttypes.models import ContentType
 from markdown_deux import markdown
 
 from comments.models import Comment
+from .utils import get_read_time
 
 
 class PostManager(models.Manager):
@@ -35,6 +36,7 @@ class Post(models.Model):
     content = models.TextField()
     draft = models.BooleanField(default=False)
     publish = models.DateField(auto_now=False, auto_now_add=False)
+    read_time = models.IntegerField(default=0)  # assume minutes not TimeField(null=True, blank=True)
     last_updated = models.DateTimeField(auto_now=True, auto_now_add=False)
     init_timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
 
@@ -81,6 +83,11 @@ def create_slug(instance, new_slug=None):
 def pre_save_post_receiver(sender, instance, *args, **kwargs):
     if not instance.slug:
         instance.slug = create_slug(instance)
+
+    if instance.content:
+        html_string = instance.get_markdown()
+        read_time_var = get_read_time(html_string)
+        instance.read_time = read_time_var
 
 
 pre_save.connect(pre_save_post_receiver, sender=Post)
--- posts/views.py
+++ posts/views.py
@@ -12,7 +12,6 @@ from .models import Post
 from .forms import PostForm
 from comments.models import Comment
 from comments.forms import CommentForm
-from .utils import get_read_time
 
 
 def post_create(request):
@@ -38,7 +37,6 @@ def post_detail(request, slug=None):  # retrieve
             raise Http404
     share_string = quote_plus(instance.content)
 
-    print(get_read_time(instance.get_markdown()))
 
     initial_data = {
         "content_type": instance.get_content_type,

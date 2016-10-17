8707846df11ff4a55e3e56fe3a0abce8eeab3d26
Tim Le <TimLe1@Live.com>
2016-10-17 13:22:31 -0400

    AtB 16, Reply to comment using the same comment crispy form

--- comments/models.py
+++ comments/models.py
@@ -5,10 +5,14 @@ from django.contrib.contenttypes.models import ContentType
 
 
 class CommentManager(models.Manager):
+    def all(self):
+        qs = super(CommentManager, self).filter(parent=None)
+        return qs
+
     def filter_by_instance(self, instance):
         content_type = ContentType.objects.get_for_model(instance.__class__)
         obj_id = instance.id
-        qs = super(CommentManager, self).filter(content_type=content_type, object_id=obj_id)
+        qs = super(CommentManager, self).filter(content_type=content_type, object_id=obj_id).filter(parent=None)
         return qs
 
 
@@ -20,10 +24,25 @@ class Comment(models.Model):
     object_id = models.PositiveIntegerField(null=True)
     content_object = GenericForeignKey('content_type', 'object_id')
 
+    parent = models.ForeignKey('self', null=True, blank=True)
+
+
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
     objects = CommentManager()
 
+    class Meta:
+        ordering = ['-timestamp']
+
     def __str__(self):
         return str(self.user.username)
+
+    def children(self):
+        return Comment.objects.filter(parent=self)
+
+    @property
+    def is_parent(self):
+        if self.parent is not None:
+            return False
+        return True

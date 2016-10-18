837a2aa03f957d9f6f17f1be8eab726a9f6bb7a3
Tim Le <TimLe1@Live.com>
2016-10-17 20:30:43 -0400

    AtB 24, login form and logout view

--- accounts/views.py
+++ accounts/views.py
@@ -14,6 +14,10 @@ def login_view(request):
     if form.is_valid():
         username = form.cleaned_data.get("username")
         password = form.cleaned_data.get('password')
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        print(request.user.is_authenticated())
+        # redirect
 
     return render(request, "accounts_form.html", {"form":form, "title": title})
 
@@ -23,4 +27,5 @@ def register_view(request):
 
 
 def logout_view(request):
+    logout(request)
     return render(request, "accounts_form.html", {})

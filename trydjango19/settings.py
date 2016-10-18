b88378ab1fcb0464fb74ca31399d416b138de1d2
Tim Le <tim.le1@live.com>
2016-10-17 22:33:48 -0400

    API 4, install djangorestframework and created posts/api package see installation instruction http://www.django-rest-framework.org/

--- trydjango19/settings.py
+++ trydjango19/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'pagedown',
     'posts',
     'comments',
+    'rest_framework',
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
--- trydjango19/urls.py
+++ trydjango19/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     url(r'^login/', login_view, name='login'),
     url(r'^logout/', logout_view, name='logout'),
     url(r'^', include('posts.urls', namespace='posts')),
+    url(r'^api/posts/', include("posts.api.urls", namespace='posts-api')),
 ]
 
 from django.conf import settings

7aba755e7cc335d81a6288e0ad540ff583a5be18
Tim Le <TimLe1@Live.com>
2016-10-17 20:17:21 -0400

    AtB 23, startapp accounts for user auth and registration

---
+++ accounts/views.py
@@ -0,0 +1,26 @@
+from django.contrib.auth import (
+    authenticate,
+    get_user_model,
+    login,
+    logout,
+)
+from django.shortcuts import render
+from .forms import UserLoginForm
+
+
+def login_view(request):
+    title = "Login"
+    form = UserLoginForm(request.POST or None)
+    if form.is_valid():
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get('password')
+
+    return render(request, "accounts_form.html", {"form":form, "title": title})
+
+
+def register_view(request):
+    return render(request, "accounts_form.html", {})
+
+
+def logout_view(request):
+    return render(request, "accounts_form.html", {})
--- trydjango19/urls.py
+++ trydjango19/urls.py
@@ -15,11 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from accounts.views import (login_view, register_view, logout_view)
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^comments/', include('comments.urls', namespace='comments')),
     url(r'^posts/', include('posts.urls', namespace='posts')),
+    url(r'^login/', login_view, name='login'),
+    url(r'^logout/', logout_view, name='logout'),
 ]
 
 from django.conf import settings

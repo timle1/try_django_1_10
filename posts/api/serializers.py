a1986c05d33812ddb54f59474a3a902c30bfdcd4
Tim Le <TimLe1@Live.com>
2016-10-18 01:24:18 -0400

    API 7, test update and delete in shell

--- posts/api/serializers.py
+++ posts/api/serializers.py
@@ -49,4 +49,24 @@ if new_item.is_valid():
 else:
     print(new_item.errors)
 
+
+#session2
+
+from posts.models import Post
+from posts.api.serializers import PostDetailSerializer
+data = {
+    "title": "Yeahh buddy",
+    "content": "New content",
+    "publish": "2016-2-12",
+    "slug": "yeah-buddy",
+
+}
+
+obj = Post.objects.get(id=2)
+new_item = PostDetailSerializer(obj, data=data)
+if new_item.is_valid():
+    new_item.save()
+else:
+    print(new_item.errors)
+
 """

693fb2106645e1c4b0ef9e1ab39a04eedbe803e2
Tim Le <TimLe1@Live.com>
2016-10-18 00:38:45 -0400

    API 5, play with serializer in django shell

--- posts/api/serializers.py
+++ posts/api/serializers.py
@@ -13,3 +13,32 @@ class PostSerializer(ModelSerializer):
             'content',
             'publish',
         ]
+
+
+
+
+
+"""
+
+from posts.models import Post
+from posts.api.serializers import PostSerializer
+obj = Post.objects.first()
+print(obj)
+
+obj_data = PostSerializer(obj)
+obj_data.data
+
+data5 = {
+    "title": "Yeahh buddy",
+    "content": "New content",
+    "publish": "2016-2-12",
+    "slug": "yeah-buddy",
+
+}
+new_item = PostSerializer(data=data5)
+if new_item.is_valid():
+    new_item.save()
+else:
+    print(new_item.errors)
+
+"""

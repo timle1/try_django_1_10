a1fc9982f0ef42bfd29ee9bf4e0005d1ba8726e7
timle1 <timle1@users.noreply.github.com>
2016-10-24 01:07:42 -0400

    API 26, UserCreateSerializer create()

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -23,3 +23,15 @@ class UserCreateSerializer(ModelSerializer):
         extra_kwargs = {"password":
                             {"write_only": True}
                             }
+
+    def create(self, validated_data):
+        username = validated_data['username']
+        email = validated_data['email']
+        password = validated_data['password']
+        user_obj = User(
+            username = username,
+            email = email
+        )
+        user_obj.set_password(password)
+        user_obj.save()
+        return validated_data

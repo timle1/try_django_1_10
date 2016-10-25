59ba4c68e5988e00d09ba2a0f7a21326821a9b8d
timle1 <timle1@users.noreply.github.com>
2016-10-25 08:20:31 -0400

    API 27,  Serializer Validation

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -2,6 +2,7 @@ from django.contrib.contenttypes.models import ContentType
 from django.contrib.auth import get_user_model
 
 from rest_framework.serializers import (
+    EmailField,
     HyperlinkedIdentityField,
     ModelSerializer,
     SerializerMethodField,
@@ -13,16 +14,47 @@ User = get_user_model()
 
 
 class UserCreateSerializer(ModelSerializer):
+    email = EmailField(label='Email Address')
+    email2 = EmailField(label='Confirm Email')
     class Meta:
         model = User
         fields = [
             'username',
             'password',
             'email',
+            'email2',
         ]
         extra_kwargs = {"password":
                             {"write_only": True}
-                            }
+                        }
+
+    def validate(self, data):
+        # email = data['email']
+        # user_qs = User.objects.filter(email=email)
+        # if user_qs.exists():
+        #     raise ValidationError("This user has already registered.")
+        return data
+
+    def validate_email(self, value):
+        data = self.get_initial()
+        email1 = data.get("email2")
+        email2 = value
+        if email1 != email2:
+            raise ValidationError("Emails must match.")
+
+        user_qs = User.objects.filter(email=email2)
+        if user_qs.exists():
+            raise ValidationError("This user has already registered.")
+
+        return value
+
+    def validate_email2(self, value):
+        data = self.get_initial()
+        email1 = data.get("email")
+        email2 = value
+        if email1 != email2:
+            raise ValidationError("Emails must match.")
+        return value
 
     def create(self, validated_data):
         username = validated_data['username']

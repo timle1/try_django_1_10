f7455d44531000d18acef695dbe7938890bcbd91
timle1 <timle1@users.noreply.github.com>
2016-10-25 13:52:52 -0400

    API 28, started UserLoginAPIView

--- accounts/api/urls.py
+++ accounts/api/urls.py
@@ -3,8 +3,10 @@ from django.contrib import admin
 
 from .views import (
     UserCreateAPIView,
+    UserLoginAPIView,
     )
 
 urlpatterns = [
+    url(r'^login/$', UserLoginAPIView.as_view(), name='logi'),
     url(r'^register/$', UserCreateAPIView.as_view(), name='register'),
 ]
